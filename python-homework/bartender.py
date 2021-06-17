24da972644d47e9bd1c844af4bb8d49081e665d3
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-17 13:32:40 +0200

    add hf start

---
+++ python-homework/bartender.py
@@ -0,0 +1,9 @@
+age = int(input("Hány éves vagy?"))
+print(age)
+a = input("sör")
+b = input("kóla")
+drink = str(input("Mit szeretnél inni? Sör vagy kóla?"))
+print(drink)
+
+If int(age) < 18
+    print("Sajnos nem adhatok")
