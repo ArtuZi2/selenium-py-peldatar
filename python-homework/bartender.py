2d86f8bcf6cfce296957c66897116b9eff45e11c
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-18 20:03:29 +0200

    add hf start

--- python-homework/bartender.py
+++ python-homework/bartender.py
@@ -1,9 +1,6 @@
 age = int(input("Hány éves vagy?"))
-print(age)
-a = input("sör")
-b = input("kóla")
-drink = str(input("Mit szeretnél inni? Sör vagy kóla?"))
-print(drink)
 
-If int(age) < 18
+If age >= 18:
+    print("Kérsz sört?"")
+else:
     print("Sajnos nem adhatok")
--- python-homework/charsandord.py
+++ python-homework/charsandord.py
@@ -1,6 +1,18 @@
-x = ord('A')
+x = ord('a')
 print(x)
 
-print(ord('A + 10'))
-print(ord('ç'))
-print(ord('$'))
+for i in range(10):
+    x += 10
+print(x)
+
+print("\x45")
+print("alma\tkörte")
+
+print("a\tb\tc", "\nd\te\tf", "\ng\th\ti", "\nj\tk\tl", "\nm\tn\to", "\np\tr\ts", "\nt\tv\tw", "\nx\ty\tz")
+
+a = ord('a')
+
+print("a\ta+10\ti**3\ti**5\ti**10\ti**20")
+for a in range(1, 11):
+    print(a, "\t", i**2, "\t", i**3, "\t", i**5, "\t",
+                                            i**10, "\t", i**20, sep='')
