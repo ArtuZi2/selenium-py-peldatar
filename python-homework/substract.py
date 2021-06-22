37eaa0d6d074081be45c953e7a59a9e5e1ad7c31
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-22 13:24:52 +0200

    substract ready

--- python-homework/substract.py
+++ python-homework/substract.py
@@ -1,3 +1,14 @@
-x = input()
-y = input ()
-print(int(x) - int(y))
+orszaguton = int(input("Mennyit fogyaszt országúton az autód?"))
+varosban = int(input("Mennyit fogyaszt városban az autód?"))
+
+orszaguton_km = int(input("Hány kilométert utaztál országúton?"))
+varosban_km = int(input("Hány kilométert utaztál városban?"))
+
+fogyasztas_oda = (orszaguton_km / 100 * orszaguton) + (varosban_km / 100 * varosban)
+print(fogyasztas_oda)
+
+fogyasztas_oda_vissza = ((orszaguton_km / 100 * orszaguton) + (varosban_km / 100 * varosban)) * 2
+print(fogyasztas_oda_vissza)
+
+teljes_ut_ara = fogyasztas_oda_vissza * 350
+print(teljes_ut_ara)
