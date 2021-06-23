b8263a6219174313477094fbe92a9bf5743d4cc2
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-23 15:50:10 +0200

    hazik modositva

--- python-homework/szokoev.py
+++ python-homework/szokoev.py
@@ -2,12 +2,16 @@
 
 def szokoev_kiiratas(evszam):
     return evszam
-
 evszam = int(input("Írj be egy évszámot!"))
+
+
+# evszam = int(input("Írj be egy évszámot!"))
 if (evszam % 4) == 0:
     print("Szökőév")
-elif (evszam % 100) != 0:
-    print("Nem szökőév")
+elif (evszam % 100) == 0:
+        print("Nem szökőév")
+elif (evszam % 400) == 0:
+    print("Szökőév")
 else:
     print("Nem szökőév")
 
---
+++ python2-homework/fordito.py
@@ -0,0 +1,7 @@
+numbers = []
+number = None
+while number != 0:
+    number = int(input("Írj be egy számot!\t"))
+    if number != 0:
+        numbers.append(number)
+print(numbers)
