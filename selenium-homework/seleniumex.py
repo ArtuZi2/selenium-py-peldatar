5f736c8c0c795f7f46e0ebcbc4e595f41b3b709e
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-24 22:59:01 +0200

    modify seleniumex.py

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
-
 driver.get("https:/python.org")
 nemletezik = driver.find_element_by_id("nemletezik")
-
-driver.close()
+try:
+    nemletezik = driver.find_element_by_id("nemletezik")
+except NoSuchElementException:
+    Print("Nincs ilyen elem")
