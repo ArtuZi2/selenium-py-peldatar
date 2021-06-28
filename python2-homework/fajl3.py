900a2aed845a5ad10ba7fe0851bb5e93b03d715f
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-28 22:37:22 +0200

    add requirements.txt

--- python2-homework/fajl3.py
+++ python2-homework/fajl3.py
@@ -2,6 +2,6 @@ with open("adat.txt", "r") as f:
     result = f.readlines()
     print(result)
 with open("adat2.txt", "a") as k:
-    k.write("Egyszer volt, hol nem volt, vol egyszer egy ember")
+    k.write("Egyszer volt, hol nem volt, volt egyszer egy ember")
     result2 = k.readlines()
-    print(k)
+    print(result2)
