7d082bbdcb3edfa46aae0778d070a2d168d68634
Mazurka Zita <tmazurkazita@gmail.com>
2021-06-28 12:37:26 +0200

    modify challange.py

--- selenium-homework/challange.py
+++ selenium-homework/challange.py
@@ -2,12 +2,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://brave-desert-0a706e303.azurestaticapps.net")
+
 def challange():
-
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://brave-desert-0a706e303.azurestaticapps.net/")
-
-
     message = "hellobello"
     user_message = driver.find_element_by_id("user_message").send_keys(message)
     driver.find_element_by_xpath('//*[@id="get-input"]/button').click()
@@ -17,5 +16,5 @@ def challange():
     b = 2
     driver.find_element_by_id("sum1").send_keys(a)
     driver.find_element_by_id("sum2").send_keys(b)
-    driver.find_element_by_xpath('//*[@id="gettotal"]/button')
+    driver.find_element_by_xpath('//*[@id="gettotal"]/button').click()
     assert int(driver.find_element_by_id('displayvalue').text) == a + b
