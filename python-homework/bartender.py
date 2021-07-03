74d2586f2d8b5d491a50a00ca82d687026ea6702
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-03 15:51:37 +0200

    add ezaz

--- python-homework/bartender.py
+++ python-homework/bartender.py
@@ -1,23 +1,15 @@
-age = int(input("Hány éves vagy?"))
-kerdes = str(input("Mit iszol?"))
+while True:
+    age = int(input("Hány éves vagy?"))
 
-if age < 18 and kerdes == 'Sört' :
-    print("Sajnos nem adhatok")
-else:
-    print("Parancsoljon, a söre")
+    kerdes = input("Mit iszol? (Sört vagy kólát?)")
 
-if age >= 60 and kerdes == 'kólát':
-    print("Megemelheti a vérnyomását!")
+    if kerdes != 'sört' and kerdes != 'Kólat':
+        print("Csak sört és kólát tudunk adni.")
+        exit()
 
-if kerdes != 'sört' and kerdes != 'kólát':
-    print("Csak sört és kólát tudunk adni")
-
-
-# else:
-    #print("Parancsoljon, a söre!")
-    #print("Parancsoljon, a kólája!")
-
-# if age >= 18 and age > 60 and kerdes == 'sört':
-    #print("Parancsoljon, a söre!")
-# if age <= 18 and age < 60 or kerdes == 'kólát':
-    #print("Parancsoljon, a kólája!")
+    if age < 18 and kerdes == 'sört':
+        print("Sajnos nem adhatok")
+    elif age >= 60 and kerdes == 'kólát':
+        print("Megemelheti a vérnyomását!")
+    else:
+        print(f'Parancsoljon, itt a {kerdes}')
