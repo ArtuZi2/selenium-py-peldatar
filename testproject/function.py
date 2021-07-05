7846012d9d5e1d4a4b1fcf376d4f7de4ba7a5105
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-05 17:56:22 +0200

    add ezaz

--- testproject/function.py
+++ testproject/function.py
@@ -2,12 +2,17 @@ def my_func(a, b, c):  # position based parameter passing
     print(a, b, c)
     return a
 
-def my_func(a,b,c=1): # default value
-    print(a,b,c)
+
+my_func(4, 3, 1)
+
+
+def my_func(a, b, c=1):  # default value
+    print(a, b, c)
+
 
 my_func(b=1, a=2)
 
-l = [1,2,3]
+"""l = [1,2,3]
 t = (1,2,3)
 # s = set(1,2,3)
 
@@ -26,4 +31,4 @@ def my_print(*args, **kwargs):
 
 my_print(1,23,3,3,3,end='-', sep='|')
 print(1,23,3,3,3)
-# print(1,2,3,4,54,5,656,7,8,89,9,9,9,98,9,9,9)
+# print(1,2,3,4,54,5,656,7,8,89,9,9,9,98,9,9,9)"""
