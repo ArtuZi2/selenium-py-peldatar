954e2a5455b8d21f0a7711eaa9cda002495812b6
Mazurka Zita <tmazurkazita@gmail.com>
2021-07-06 09:13:57 +0200

    add csvszures.py

--- selenium2-homework/csvszures.py
+++ selenium2-homework/csvszures.py
@@ -1,6 +1,5 @@
 import csv
 
-#row = table_in_csv[table_in_csv.find('Email'):table_in_csv.find('name')]
 with open("table_in_csv", encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     next(csvreader)
